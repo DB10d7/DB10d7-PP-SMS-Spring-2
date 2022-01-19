836471028282aaf400c7db9dd5cf4ba7ee104e2c
Debdeep Banerjee <banerjeedebdeep0@gmail.com>
2022-01-19 17:50:39 +0530

    deploy

--- src/main/java/com/packetprep/system/Model/User.java
+++ src/main/java/com/packetprep/system/Model/User.java
@@ -19,7 +19,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name= "pp_user")
+//@Table(name= "pp_user")
 public class User {
     @Id
     @GeneratedValue(strategy = IDENTITY)
